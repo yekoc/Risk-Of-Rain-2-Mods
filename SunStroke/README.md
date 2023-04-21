32515e290e96ff14babf8c6f52bff6f2a0b7ee06
yekoc <67821857+yekoc@users.noreply.github.com>
2023-04-22 01:23:43 +0300

    New Mod: SunStroke

---
+++ SunStroke/README.md
@@ -0,0 +1,21 @@
+#What?
+---
+Makes it so that Overheat from the grandparent sun(and other things that use the same effect) affects all sources of burn equally,instead of being tracked by the source for manual application.\
+Might be overpowered if another balance mod accidentally undoes the thing that prevents the grandparents from double dipping into the boost.\
+You can also manually bring that upon yourself in the config,if you want to,or find yourself needing to for compatibility reasons.
+
+#Whats with the Name?
+---
+The buff is named Overheat,it's applied by a Sun,you can figure it out.
+
+#Why?
+---
+It was disappointing that it only applied to The Unmatched Power of The Sun. Also Paladin synergy.\
+Also also, I really wanted to make this icon edit.
+
+Credit to the GNU image manipulation program for helping me make the icon.\
+Credit to cute anime girls for giving me the will to continue living.
+
+#Changelog
+---
+1.0.0 - Initial Release
---
+++ SunStroke/SunStroke/SunStroke.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using RoR2.Skills;
+using UnityEngine;
+using System.Security;
+using System.Security.Permissions;
+using System.Collections.Generic;
+using static RoR2.DotController.DotIndex;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+#pragma warning disable CS0618
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace SunStroke
+{
+    [BepInPlugin("xyz.yekoc.SunStroke", "Unmatched Power of the Sun's Heat","1.0.0" )]
+    [BepInDependency("com.rob.Paladin",BepInDependency.DependencyFlags.SoftDependency)]
+    public class SunStrokePlugin : BaseUnityPlugin
+    {
+        public ConfigEntry<bool> vanillaSun;
+	
+	private void Awake()
+        {
+            vanillaSun = Config.Bind("Balance/Compat","Vanilla Sun Behavior",false,"If set to true,doesn't apply the hook that prevents the sun from applying extra burn.Set for compatibility or to buff grandparents.Default Value: False");
+            On.RoR2.StrengthenBurnUtils.CheckDotForUpgrade += (On.RoR2.StrengthenBurnUtils.orig_CheckDotForUpgrade orig,Inventory inv,ref InflictDotInfo info) =>{
+              if((info.dotIndex == Burn || info.dotIndex == StrongerBurn || info.dotIndex == Helfire) && (info.victimObject)){
+                  var stacks = Mathf.Max(1,info.victimObject.GetComponent<CharacterBody>().GetBuffCount(RoR2Content.Buffs.Overheat) - 2);
+                  info.damageMultiplier *= stacks;
+                  info.totalDamage *= stacks;
+                  Debug.Log("new burn at " + stacks);
+              }
+              orig(inv,ref info);
+            };
+            if(!vanillaSun.Value){
+              IL.RoR2.GrandParentSunController.ServerFixedUpdate += (il) =>{
+                 var c = new ILCursor(il);
+                 if(c.TryGotoNext(x => x.MatchSub(),x => x.MatchStloc(out _))){
+                     c.Index++;
+                     c.EmitDelegate<System.Func<int,int>>((orig) => Mathf.Min(orig,1));
+                 }
+              };
+              if(BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rob.Paladin")){
+                  PaladinCompat();
+              }
+            }
+	}
+
+        private void PaladinCompat(){
+          new MonoMod.RuntimeDetour.ILHook(typeof(PaladinSunController).GetMethod("ServerFixedUpdate",(System.Reflection.BindingFlags)(-1)),(ILContext il) => {
+             var c = new ILCursor(il);
+             int locIndex = -1;
+             if(c.TryGotoNext(MoveType.After,x => x.MatchCallOrCallvirt(typeof(CharacterBody).GetMethod("GetBuffCount",new System.Type[]{typeof(BuffDef)})), x => x.MatchStloc(out locIndex),x => x.MatchLdloc(locIndex)) && c.TryGotoNext(MoveType.After,x => x.MatchLdloc(locIndex))){
+                c.EmitDelegate<System.Func<int,int>>((orig) => Mathf.Min(orig,1));
+             }
+             else{
+               Logger.LogError("Paladin compat hook failed,his sun will double dip into overheat damage");
+            }
+          });
+        }
+    }
+}
