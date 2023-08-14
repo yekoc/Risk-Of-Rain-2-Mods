c08a3d0f3545866c5dc420c82ff8802064dac895
yekoc <67821857+yekoc@users.noreply.github.com>
2023-08-14 20:48:56 +0300

    AtmosphericGenerator: Initial Release

---
+++ AtmosphericGenerator/AthmosphericGenerator/AthmosphericGenerator.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using RoR2.Skills;
+using UnityEngine;
+using System.Security;
+using System.Security.Permissions;
+using System.Collections.Generic;
+
+#pragma warning disable CS0618
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace AtmosphericShields{
+
+    [BepInPlugin("xyz.yekoc.AtmosphericShields", "Atmospheric Bubble Generator","1.0.0" )]
+    public class AtmosphericShieldsPlugin : BaseUnityPlugin{
+        public static GameObject shieldPrefab;
+
+        public static List<FogDamageController> fog = new List<FogDamageController>();
+
+	public void Awake(){
+           shieldPrefab = UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Engi/EngiBubbleShield.prefab").WaitForCompletion();
+           var zone = shieldPrefab.AddComponent<SphereZone>();
+           zone.radius = 10f;
+
+           On.RoR2.FogDamageController.Start += (orig,self) =>{
+              orig(self);
+              fog.Add(self);
+              self.gameObject.AddComponent<OnDestroyComp>();
+           };
+           
+           On.EntityStates.Engi.EngiBubbleShield.Deployed.OnEnter += (orig,self) =>{
+               orig(self);
+               var zon = self.gameObject.GetComponent<SphereZone>();
+               foreach(FogDamageController fdc in fog){
+                 fdc.AddSafeZone(zon);
+               }
+           };
+           On.EntityStates.Engi.EngiBubbleShield.Deployed.OnExit += (orig,self) =>{
+              var zon = self.gameObject.GetComponent<SphereZone>();
+              foreach(FogDamageController fdc in fog){
+                fdc.RemoveSafeZone(zon);
+              }
+              orig(self);
+           };
+	}
+
+        public class OnDestroyComp : MonoBehaviour{
+           public void OnDestroy(){
+             fog.Remove(gameObject.GetComponent<FogDamageController>());
+           }
+        }
+
+    }
+}
---
+++ AtmosphericGenerator/README.md
@@ -0,0 +1,20 @@
+#What?
+---
+Makes it so that the bubble shield generated from Engineer's base utility protects you from void fog effects while inside.
+
+#Whats with the Name?
+---
+It's a bubble,filled with it's own atmosphere.\
+Also the game kinda heavily implies that "void fog" is actually an artificial vacuum. 
+
+#Why?
+---
+Felt appropriate.
+
+
+Credit to the GNU image manipulation program for helping me make the icon.\
+Credit to cute anime girls for giving me the will to continue living.
+
+#Changelog
+---
+1.0.0 - Initial Release
